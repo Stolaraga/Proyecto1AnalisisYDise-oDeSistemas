d228deb04dad3b7083142ed96300653f9f23af14
EGUX-a <125316285+Stolaraga@users.noreply.github.com>
2025-10-01 03:43:04 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+// src/App.tsx
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import RegistrarTutor from "./pages/RegistrarTutor";
 import RegistrarProgramacion from "./pages/RegistrarProgramacion";
@@ -7,12 +8,24 @@ import AccesosPermisos from "./pages/AccesosPermisos";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/registrar-tutor" element={<RegistrarTutor />} />
-      <Route path="/registrar-programacion" element={<RegistrarProgramacion />} />
-      <Route path="/registrar-disponibilidad" element={<RegistrarDisponibilidad />} />
-      <Route path="/accesos-permisos" element={<AccesosPermisos />} />
-    </Routes>
+    <>
+      <header style={{padding:"10px 16px",borderBottom:"1px solid #e6e8ef"}}>
+        <nav style={{display:"flex",gap:12,fontFamily:"system-ui,sans-serif"}}>
+          <Link to="/">Inicio</Link>
+          <Link to="/registrar-tutor">Registrar tutor</Link>
+          <Link to="/registrar-programacion">Programación</Link>
+          <Link to="/registrar-disponibilidad">Disponibilidad</Link>
+          <Link to="/accesos-permisos">Accesos y permisos</Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registrar-tutor" element={<RegistrarTutor />} />
+        <Route path="/registrar-programacion" element={<RegistrarProgramacion />} />
+        <Route path="/registrar-disponibilidad" element={<RegistrarDisponibilidad />} />
+        <Route path="/accesos-permisos" element={<AccesosPermisos />} />
+      </Routes>
+    </>
   );
 }
